3ad8268cd568834c0a93428e1736cf02caa0ad4b
Marshall Thompson <marshallthmomp@gmail.com>
2016-10-17 22:20:20 -0400

    made a pluralize function to handle like/likes, not sure about toJSON

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -11,19 +11,22 @@ var Like = require('./models/like').Like;
   var likeButton = new Like();
   // get the DOM element we want to update
   var $likeCounter = $('.js-like-count');
-  // get the initial value of 'likes'
+  var $likeVerb = $('.js-like-verb');
+  // get the initial value of 'likes' and 'verb'
   $likeCounter.text(likeButton.get('likes'));
+  $likeVerb.text(likeButton.get('verb'));
 
   $('#like-me').on('click', function(){
     // call like method to update the model
     likeButton.like();
     // save new 'likes' count to a variable
     var likeCount = likeButton.get('likes');
+    var likeVerb = likeButton.get('verb');
     // console log some ish
-    console.log('liked: current likes =', likeCount);
+    console.log('liked: current =', likeCount, likeVerb);
     // update the DOM element
     $likeCounter.text(likeCount);
-
+    $likeVerb.text(likeVerb);
   });
 
 }(jQuery));
--- app/scripts/models/like.js
+++ app/scripts/models/like.js
@@ -4,17 +4,50 @@ var Backbone = require('backbone');
 // extend Backbone.Model
 var Like = Backbone.Model.extend({
   defaults : {
-    likes : 0
+    'likes' : 0,
+    'verb': 'Likes'
   },
   // define own initialize function
   initialize : function(){
     console.log('Like Instantiated!');
   },
-  like : function(){
+  // method to update 'likes'
+  like: function(){
     var currentLikes = this.get('likes');
     this.set('likes', currentLikes + 1);
+    this.pluralize();
+  },
+  pluralize: function(){
+    var likes = this.get('likes');
+    if(likes === 1){
+      this.set('verb', 'Like');
+    } else {
+      this.set('verb' , 'Likes');
+    }
   }
 });
+
+// check likes count and pluralize 'Like' text
+// not sure of the advantage of using toJSON in this
+// particular context? Played around with this, but
+// perhaps my google-foo wasn't on point
+Like.prototype.toJSON = function(){
+  // call in the og toJSON method from Backbone
+  var json = Backbone.Model.prototype.toJSON.call(this);
+  // will return a new object with the
+  // like or likes property, but not sure
+  // how to return that and restore the
+  // normal toJSON functionality
+  if(this.get('likes') === 1){
+    return {
+      'like': this.get('likes')
+    };
+  } else {
+    return {
+      'likes' : this.get('likes')
+    };
+  }
+};
 
 module.exports = {
   'Like' : Like
